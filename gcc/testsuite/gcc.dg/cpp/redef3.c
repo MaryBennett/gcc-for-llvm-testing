b4462e7eaa66148236951ff808c004f057ac6902
Mary Bennett <mary.bennett@embecosm.com>
2021-04-28 11:45:43 +0100

    Added a proc template to check for optimisation flag support
    gcc/testsuite/ChangeLog.Embecosm:
    
            * lib/target-supports-dg.exp (dg-require-effective-target-flag): Added
            a proc template to check whether or not an optimisation flag is
            supported by the target.
            * lib/target-supports.exp (is-effective-target-flag): Likewise.
            * gcc.dg/iec-559-macros-6.c: Requires target with support for
            optimisation flags.
            * gcc.dg/independent-cloneids-1.c: Likewise.
            * gcc.dg/nested-func-12.c: Likewise.
            * gcc.dg/pr49860.c: Likewise.
            * gcc.dg/pr51039.c: Likewise.
            * gcc.dg/pr87929.c: Likewise.
            * gcc.dg/pr88444.c: Likewise.
            * gcc.dg/pr92591-2.c: Likewise.
            * gcc.dg/pr94002.c: Likewise.
            * gcc.dg/pr94292.c: Likewise.
            * gcc.dg/pr95580.c: Likewise.
            * gcc.dg/pr97192.c: Likewise.
            * gcc.dg/pr97396.c: Likewise.
            * gcc.dg/single-precision-constant.c: Likewise.
            * gcc.dg/sms-compare-debug-1.c: Likewise.
            * gcc.dg/type-convert-var.c: Likewise.
            * gcc.dg/var-expand2.c: Likewise.

--- gcc/testsuite/gcc.dg/cpp/redef3.c
+++ gcc/testsuite/gcc.dg/cpp/redef3.c
@@ -1,5 +1,4 @@
 /* Test for redefining macros with mismatch token count (and the oddity). */
-
 /* { dg-do preprocess } */
 /* { dg-options "-DC -DD=1 -DE" } */
 /* { dg-require-effective-target gcc_frontend } */
@@ -12,11 +11,11 @@
 #define D 1 2
 #define E
 
-/* { dg-warning "-:redefined" "redef A"      { target *-*-* } 8  }
-   { dg-warning "-:redefined" "redef B"      { target *-*-* } 10 }
-   { dg-warning "-:redefined" "redef D"      { target *-*-* } 12 }
-   { dg-warning "-:redefined" "redef E"      { target *-*-* } 13 }
-   { dg-message "-:previous"  "prev def A"   { target *-*-* } 7  }
-   { dg-message "-:previous"  "prev def B"   { target *-*-* } 9  }
+/* { dg-warning "-:redefined" "redef A"      { target *-*-* } 7  }
+   { dg-warning "-:redefined" "redef B"      { target *-*-* } 9  }
+   { dg-warning "-:redefined" "redef D"      { target *-*-* } 11 }
+   { dg-warning "-:redefined" "redef E"      { target *-*-* } 12 }
+   { dg-message "-:previous"  "prev def A"   { target *-*-* } 6  }
+   { dg-message "-:previous"  "prev def B"   { target *-*-* } 8  }
    { dg-message "-:previous"  "prev def D/E" { target *-*-* } 0  }
 */
